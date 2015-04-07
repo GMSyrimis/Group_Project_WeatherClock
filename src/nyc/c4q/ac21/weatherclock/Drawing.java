91e104fcea7d35d39006869b1a67ad50449420b0
GMSyrimis <gmsyrimis@gmail.com>
2015-04-06 20:36:06 -0400

    George Commit, alphabet, numbers, printer

---
+++ src/nyc/c4q/ac21/weatherclock/Drawing.java
@@ -0,0 +1,266 @@
+package nyc.c4q.ac21.weatherclock;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by c4q-george on 4/5/15.
+ */
+
+public class Drawing {
+
+
+    public static ArrayList<String> getPrintLines(String rawWeekday){
+
+        ArrayList<String> fullPrint =  new ArrayList<String>();
+
+        String firstLine =  "";
+        String secondLine = "";
+        String thirdLine =  "";
+        String fourthLine = "";
+        String fifthLine =  "";
+
+        for(int i =0; i<rawWeekday.length(); i++){
+
+            char rawLetter = rawWeekday.charAt(i);
+
+            if(rawLetter == 'a'){
+                firstLine +=" "+"         "+" ";
+                secondLine+=" "+"         "+" ";
+                thirdLine+= " "+"███████  "+" ";
+                fourthLine+=" "+"██   ██ █"+" ";
+                fifthLine+= " "+"█████████"+" ";
+            }
+            if(rawLetter == 'b'){
+                firstLine +=" "+"██     "+" ";
+                secondLine+=" "+"██     "+" ";
+                thirdLine+= " "+"███████"+" ";
+                fourthLine+=" "+"██   ██"+" ";
+                fifthLine+= " "+"███████"+" ";
+            }
+            if(rawLetter == 'c') {
+                firstLine+=  " "+"      "+" ";
+                secondLine+= " "+"      "+" ";
+                thirdLine+=  " "+"██████"+" ";
+                fourthLine+= " "+"██    "+" ";
+                fifthLine+=  " "+"██████"+" ";
+            }
+            if(rawLetter == 'd'){
+                firstLine +=  " "+"     ██"+" ";
+                secondLine += " "+"     ██"+" ";
+                thirdLine +=  " "+"███████"+" ";
+                fourthLine += " "+"██   ██"+" ";
+                fifthLine+=   " "+"███████"+" ";
+            }
+            if(rawLetter == 'e'){
+                firstLine +=  " "+"       "+" ";
+                secondLine += " "+"  ███  "+" ";
+                thirdLine +=  " "+"██   ██"+" ";
+                fourthLine += " "+"██ ██  "+" ";
+                fifthLine +=  " "+" █████ "+" ";
+            }
+            if(rawLetter == 'f'){
+                firstLine +=  " "+" █████ "+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+"████   "+" ";
+                fourthLine += " "+"██     "+" ";
+                fifthLine +=  " "+"██     "+" ";
+            }
+            if(rawLetter == 'g'){
+                firstLine +=  " "+" █████ "+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+" ██████"+" ";
+                fourthLine += " "+"     ██"+" ";
+                fifthLine +=  " "+"  ████ "+" ";
+            }
+            if(rawLetter == 's'){
+                firstLine +=  " "+" █████ "+" ";
+                secondLine += " "+"██     "+" ";
+                thirdLine +=  " "+" █████ "+" ";
+                fourthLine += " "+"     ██"+" ";
+                fifthLine +=  " "+" █████ "+" ";
+            }
+            if(rawLetter == 'h'){
+                firstLine +=" "+"██     "+" ";
+                secondLine+=" "+"██     "+" ";
+                thirdLine+= " "+"███████"+" ";
+                fourthLine+=" "+"██   ██"+" ";
+                fifthLine+= " "+"██   ██"+" ";
+            }
+            if(rawLetter == 'i'){
+                firstLine +=" "+"██"+" ";
+                secondLine+=" "+"  "+" ";
+                thirdLine+= " "+"██"+" ";
+                fourthLine+=" "+"██"+" ";
+                fifthLine+= " "+"██"+" ";
+            }
+            if(rawLetter == 'r'){
+                firstLine +=" "+"      "+" ";
+                secondLine+=" "+"      "+" ";
+                thirdLine+= " "+"█████ "+" ";
+                fourthLine+=" "+"██  ██"+" ";
+                fifthLine+= " "+"██    "+" ";
+            }
+            if(rawLetter == 'u'){
+                firstLine +=  " "+"       "+" ";
+                secondLine += " "+"       "+" ";
+                thirdLine +=  " "+"██   ██"+" ";
+                fourthLine += " "+"██   ██"+" ";
+                fifthLine+=   " "+"███████"+" ";
+            }
+            if(rawLetter == 't'){
+                firstLine +=  " "+"      "+" ";
+                secondLine += " "+"  ██  "+" ";
+                thirdLine +=  " "+"██████"+" ";
+                fourthLine += " "+"  ██  "+" ";
+                fifthLine+=   " "+"  ████"+" ";
+            }
+            if(rawLetter == 'm'){
+                firstLine +=  " "+"       "+" ";
+                secondLine += " "+"█      "+" ";
+                thirdLine +=  " "+"███████"+" ";
+                fourthLine += " "+"██ ██ █"+" ";
+                fifthLine+=   " "+"██ ██ █"+" ";
+            }
+            if(rawLetter == 'n'){
+                firstLine +=" "+"       "+" ";
+                secondLine+=" "+"█      "+" ";
+                thirdLine+= " "+"███████"+" ";
+                fourthLine+=" "+"██   ██"+" ";
+                fifthLine+= " "+"██   ██"+" ";
+            }
+            if(rawLetter == 'o'){
+                firstLine +=" "+"       "+" ";
+                secondLine+=" "+"       "+" ";
+                thirdLine+= " "+"███████"+" ";
+                fourthLine+=" "+"██   ██"+" ";
+                fifthLine+= " "+"███████"+" ";
+            }
+            if(rawLetter == 'p'){
+                firstLine +=  " "+"██████ "+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+"██████ "+" ";
+                fourthLine += " "+"██     "+" ";
+                fifthLine +=  " "+"██     "+" ";
+            }
+            if(rawLetter == 'v'){
+                firstLine +=" "+"      "+" ";
+                secondLine+=" "+"      "+" ";
+                thirdLine+= " "+"██  ██"+" ";
+                fourthLine+=" "+" █  █ "+" ";
+                fifthLine+= " "+"  ██  "+" ";
+            }
+            if(rawLetter == 'y'){
+                firstLine +=  " "+"██   ██"+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+" ██████"+" ";
+                fourthLine += " "+"     ██"+" ";
+                fifthLine +=  " "+"  ████ "+" ";
+            }
+            if(rawLetter == '0'){
+                firstLine +=  " "+"███████"+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+"██   ██"+" ";
+                fourthLine += " "+"██   ██"+" ";
+                fifthLine +=  " "+"███████"+" ";
+            }
+            if(rawLetter == '1'){
+                firstLine +=  " "+"██"+" ";
+                secondLine += " "+"██"+" ";
+                thirdLine +=  " "+"██"+" ";
+                fourthLine += " "+"██"+" ";
+                fifthLine +=  " "+"██"+" ";
+            }
+            if(rawLetter == '2'){
+                firstLine +=  " "+" ████  "+" ";
+                secondLine += " "+"█    ██"+" ";
+                thirdLine +=  " "+"   ██  "+" ";
+                fourthLine += " "+" ██    "+" ";
+                fifthLine +=  " "+"███████"+" ";
+            }
+            if(rawLetter == '3'){
+                firstLine +=  " "+" █████ "+" ";
+                secondLine += " "+"█    ██"+" ";
+                thirdLine +=  " "+"   ███ "+" ";
+                fourthLine += " "+"█    ██"+" ";
+                fifthLine +=  " "+" █████ "+" ";
+            }
+            if(rawLetter == '4'){
+                firstLine +=  " "+"██   ██  "+" ";
+                secondLine += " "+"██   ██  "+" ";
+                thirdLine +=  " "+"█████████"+" ";
+                fourthLine += " "+"     ██  "+" ";
+                fifthLine +=  " "+"     ██  "+" ";
+            }
+            if(rawLetter == '5'){
+                firstLine +=  " "+"███████"+" ";
+                secondLine += " "+"██     "+" ";
+                thirdLine +=  " "+"██████ "+" ";
+                fourthLine += " "+"     ██"+" ";
+                fifthLine +=  " "+"██████ "+" ";
+            }
+            if(rawLetter == '6'){
+                firstLine +=  " "+"███████"+" ";
+                secondLine += " "+"██     "+" ";
+                thirdLine +=  " "+"███████"+" ";
+                fourthLine += " "+"██   ██"+" ";
+                fifthLine +=  " "+"███████"+" ";
+            }
+            if(rawLetter == '7'){
+                firstLine +=  " "+"████████"+" ";
+                secondLine += " "+"█    ██ "+" ";
+                thirdLine +=  " "+"    ██  "+" ";
+                fourthLine += " "+"   ██   "+" ";
+                fifthLine +=  " "+"  ██    "+" ";
+            }
+            if(rawLetter == '8'){
+                firstLine +=  " "+"███████"+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+"███████"+" ";
+                fourthLine += " "+"██   ██"+" ";
+                fifthLine +=  " "+"███████"+" ";
+            }
+            if(rawLetter == '9'){
+                firstLine +=  " "+"███████"+" ";
+                secondLine += " "+"██   ██"+" ";
+                thirdLine +=  " "+"███████"+" ";
+                fourthLine += " "+"     ██"+" ";
+                fifthLine +=  " "+"███████"+" ";
+            }
+            if(rawLetter == ':'){
+                firstLine +=  " "+"  "+" ";
+                secondLine += " "+"██"+" ";
+                thirdLine +=  " "+"  "+" ";
+                fourthLine += " "+"██"+" ";
+                fifthLine +=  " "+"  "+" ";
+            }
+            if(rawLetter == '-'){
+                firstLine +=  " "+"    "+" ";
+                secondLine += " "+"    "+" ";
+                thirdLine +=  " "+"████"+" ";
+                fourthLine += " "+"    "+" ";
+                fifthLine +=  " "+"    "+" ";
+            }
+        }
+
+        fullPrint.add(firstLine);
+        fullPrint.add(secondLine);
+        fullPrint.add(thirdLine);
+        fullPrint.add(fourthLine);
+        fullPrint.add(fifthLine);
+
+
+        return fullPrint;
+    }
+
+
+    public static void main(String[] args){
+        ArrayList<String> fullPrint;
+
+        fullPrint = getPrintLines("thursday");
+
+        for (String lines : fullPrint){
+            System.out.println(lines);
+        }
+    }
+}
--- src/nyc/c4q/ac21/weatherclock/Main.java
+++ src/nyc/c4q/ac21/weatherclock/Main.java
@@ -4,38 +4,54 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class Main {
 
     /**
      * SAMPLE CODE: Returns sunset time for the current day.
      */
-    public static Calendar getSunset() {
-        URL url = HTTP.stringToURL("http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY");
-        String doc = HTTP.get(url);
-        JSONObject obj = (JSONObject) JSONValue.parse(doc);
 
+    // method that returns a calendar with the sunsetTimestamp
+    public static Calendar getSunset() {
+        //Change the New%20York,NY to a user defined variable
+        // Creating a URL object to the JSON generator
+        URL url = HTTP.stringToURL("http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY");
+        // Goes to the webpage and saves the JSON to a String
+        String doc = HTTP.get(url);
+        // we create a JSONObject called obj and
+        // we typecast JSONValue.parse(doc) to JSONObject
+        JSONObject obj = (JSONObject) JSONValue.parse(doc);
+        // New JSONObject called sys
+        // This we call get on obj
         JSONObject sys = (JSONObject) obj.get("sys");
+        // check if out sys JSONObject is null
         if (sys == null)
             return null;
+        // Getting the sunsetTimestamp from sys
         Long sunsetTimestamp = (Long) sys.get("sunset");
+        // Checking if the timestamp is null
         if (sunsetTimestamp == null)
             return null;
+        // Returning a calendar with the timestamp using the DateTime class
         return DateTime.fromTimestamp(sunsetTimestamp);
     }
 
     /**
      * SAMPLE CODE: Displays a very primitive clock.
      */
+
+
     public static void main(String[] args) {
+
+//--------------- TERMINAL LOGISTICS --------------------
         // Find out the size of the terminal currently.
         final int numCols = TerminalSize.getNumColumns();
         final int numRows = TerminalSize.getNumLines();
-
         // Create the terminal.
         final AnsiTerminal terminal = new AnsiTerminal();
-
         // When the program shuts down, reset the terminal to its original state.
         // This code makes sure the terminal is reset even if you kill your
         // program by pressing Control-C.
@@ -47,43 +63,79 @@ public class Main {
                 terminal.moveTo(numRows, 0);
             }
         });
-
         // Clear the screen to black.
         terminal.setBackgroundColor(AnsiTerminal.Color.BLACK);
         terminal.clear();
         // Don't show the cursor.
         terminal.hideCursor();
+//------------- TERMINAL IS SET UP ----------------------
+
+
 
         // Get sunset time for the current day.
         Calendar sunset = getSunset();
 
         int xPosition = 1 + numCols / 2 - 5;
+        int clockPosition = 1+numCols / 4 - 5;
+
+
         while (true) {
+            //BUILDING THE SUN ARC
+            int startARC = 3;
+            int endARC = numCols-3;
+            int maxHeightARC = numRows/5;
+            String lineTEST = "";
+            for(int q=startARC; q<endARC; q++){
+                lineTEST +="+";
+            }
+            terminal.moveTo(maxHeightARC, startARC);
+            terminal.write(lineTEST);
+
             // Get the current date and time.
             Calendar cal = Calendar.getInstance();
-
+           
             // Write the time, including seconds, in white.
             String time = DateTime.formatTime(cal, true);
+
             if (cal.get(Calendar.HOUR_OF_DAY) >= 12)
                 time += " PM";
             else
                 time += " AM";
             terminal.setTextColor(AnsiTerminal.Color.WHITE);
-            terminal.moveTo(3, xPosition);
-            terminal.write(time);
+
+            ArrayList<String> fullTime;
+            fullTime = Drawing.getPrintLines(time.toLowerCase());
+
+            for (int i=0; i<fullTime.size(); i++){
+                terminal.moveTo(22+i, 2);
+                terminal.write("  " + fullTime.get(i) + "  ");
+            }
+
 
             // Write the date in gray.
             String date = DateTime.formatDate(cal);
+
             terminal.setTextColor(AnsiTerminal.Color.WHITE, false);
-            terminal.moveTo(5, xPosition);
-            terminal.write(date);
+
+            ArrayList<String> fullDate;
+            fullDate = Drawing.getPrintLines(date);
+            for (int i=0; i<fullDate.size(); i++){
+                terminal.moveTo(29+i, 2);
+                terminal.write("  " + fullDate.get(i) + "  ");
+            }
 
             // Write the day of the week in green on a blue background.
             String dayOfWeek = DateTime.getDayOfWeekNames().get(cal.get(Calendar.DAY_OF_WEEK));
+
             terminal.setTextColor(AnsiTerminal.Color.GREEN);
             terminal.setBackgroundColor(AnsiTerminal.Color.BLUE);
-            terminal.moveTo(7, xPosition);
-            terminal.write("  " + dayOfWeek + "  ");
+            // loop through the array
+            ArrayList<String> fullPrint;
+            fullPrint = Drawing.getPrintLines(dayOfWeek.toLowerCase());
+            for (int i=0; i<fullPrint.size(); i++){
+                terminal.moveTo(37+i, 2);
+                terminal.write("  " + fullPrint.get(i) + "  ");
+            }
 
             // Set the background color back to black.
             terminal.setBackgroundColor(AnsiTerminal.Color.BLACK);
@@ -91,7 +143,7 @@ public class Main {
             // Write sunset time in dark yellow.
             String sunsetTime = DateTime.formatTime(sunset, false);
             terminal.setTextColor(AnsiTerminal.Color.YELLOW, false);
-            terminal.moveTo(9, xPosition - 2);
+            terminal.moveTo(21, 2);
             terminal.write("sunset at " + sunsetTime);
 
             // Pause for one second, and do it again.
